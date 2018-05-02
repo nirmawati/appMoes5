8adde81580001170f6fb7ed364fbfdf2fea0931d
nirmawati <nirmawati178@gmail.com>
2018-05-02 17:01:20 +0700

    crop image

--- app/src/main/java/com/example/nirma/moes5/SettingsActivity.java
+++ app/src/main/java/com/example/nirma/moes5/SettingsActivity.java
@@ -1,17 +1,29 @@
 package com.example.nirma.moes5;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -22,6 +34,9 @@ public class SettingsActivity extends AppCompatActivity
     private TextView settingsDisplayStatus;
     private Button settingsChangeProfileImageButton;
     private Button settingsChangeStatusButton;
+
+    private final static int Gallery_Pick = 1;
+    private StorageReference storeProfileImageStorageRef;
 
     private DatabaseReference getUserDataReference;
     private FirebaseAuth mAuth;
@@ -35,6 +50,7 @@ public class SettingsActivity extends AppCompatActivity
         mAuth = FirebaseAuth.getInstance();
         String online_user_id = mAuth.getCurrentUser().getUid();
         getUserDataReference = FirebaseDatabase.getInstance().getReference().child("Users").child(online_user_id);
+        storeProfileImageStorageRef = FirebaseStorage.getInstance().getReference().child("profile_image");
 
         settingsDisplayProfileImage = (CircleImageView) findViewById(R.id.settings_profile_image);
         settingsDisplayName = (TextView) findViewById(R.id.settings_username);
@@ -62,5 +78,61 @@ public class SettingsActivity extends AppCompatActivity
 
             }
         });
+
+        settingsChangeProfileImageButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, Gallery_Pick);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Gallery_Pick && requestCode == RESULT_OK && data!=null)
+        {
+            Uri ImageUri = data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK)
+            {
+                Uri resultUri = result.getUri();
+                String user_id = mAuth.getCurrentUser().getUid();
+                StorageReference filePath = storeProfileImageStorageRef.child(user_id + ".jpg");
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(SettingsActivity.this, "Saving your profile image...", Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(SettingsActivity.this, "Error occuret, while uploding your profile pic..", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+            else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE)
+            {
+                Exception error = result.getError();
+            }
+        }
     }
 }
